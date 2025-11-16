3f0f941a3d9d87468b3b8b456a8b5a7e4f652575
hiteshkumar042 <hksolanki78@gmail.com>
2025-11-16 19:27:53 +0530

    cdr added

--- src/app/components/navbar-component/navbar-component.ts
+++ src/app/components/navbar-component/navbar-component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-component',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.scss',
 })
 export class NavbarComponent {
+  title = "Task Manager App"
 
+  private cdr = inject(ChangeDetectorRef)
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.title = "Task Manager App";
+      this.cdr.markForCheck()
+    }, 2000);
+
+  }
 }
