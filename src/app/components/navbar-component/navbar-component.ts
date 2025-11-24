6b39e4598e5322523ba19f44358b9f060c900279
hiteshkumar042 <hksolanki78@gmail.com>
2025-11-25 00:33:51 +0530

    router link for aos

--- src/app/components/navbar-component/navbar-component.ts
+++ src/app/components/navbar-component/navbar-component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-component',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.scss',
 })
